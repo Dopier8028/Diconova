a95c7349bb65952cfce7d58038b54b3084d41d2c
Dopier8028 <diego.rafael.peralta@gmail.com>
2026-03-23 22:03:34 -0600

    Delete Para la escuela directory

--- Para la escuela/include/breadcrumbs.php
+++
@@ -1,9 +0,0 @@
-<div class="breadcrumbs-custom bg-image novi-background bg-primary">
-    <div class="container">
-        <ul class="breadcrumbs-custom-path">
-        <li><a href="../index.html">Inicio</a></li>
-            <li><a href="<?php echo $projectLink; ?>"><?php echo $projectName; ?></a></li>
-            <li class="active"><?php echo $projectTitle; ?></li>
-        </ul>
-    </div>
-</div>
