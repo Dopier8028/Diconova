e184f62a2f2cc4ec8fb93b1b4193872f1f3e32b2
Dopier8028 <diego.rafael.peralta@gmail.com>
2026-03-23 21:25:44 -0600

    first commit

---
+++ Para la escuela/include/Proyecto3.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html class="wide wow-animation" lang="en">
+<head>
+    <title>Proyectos</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="icon" href="../images/favicon.ico" type="../image/x-icon">
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Lato:300,300i,400,400i,700,900">
+    <link rel="stylesheet" href="../css/bootstrap.css">
+    <link rel="stylesheet" href="../css/fonts.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <style>
+        .ie-panel {
+            display: none;
+            background: #212121;
+            padding: 10px 0;
+            box-shadow: 3px 3px 5px 0 rgba(0, 0, 0, .3);
+            clear: both;
+            text-align: center;
+            position: relative;
+            z-index: 1;
+        }
+        html.ie-10 .ie-panel, html.lt-ie-10 .ie-panel {
+            display: block;
+        }
+    </style>
+</head>
+<body>
+    <div class="ie-panel">
+        <a href="http://windows.microsoft.com/en-US/internet-explorer/">
+            <img src="images/ie8-panel/warning_bar_0000_us.jpg" height="42" width="820" alt="You are using an outdated browser.">
+        </a>
+    </div>
+
+    <?php include 'preloader.php'; ?>
+
+    <div class="page">
+    <?php 
+            $current_page = basename($_SERVER['PHP_SELF']); // Obtiene el nombre del archivo actual
+            $active_proyectos = ($current_page == 'Proyectos.html') ? 'active' : 'active';
+            $active_especialidades = ($current_page == 'Especialidades.html') ? 'active' : '';
+        include 'header.php'; ?>
+
+        <?php
+        $projectTitle = "Plazas de Salud (PLASALUD) en Morelos, Guerrero y Ciudad de México";  
+        $projectLink = "../Proyectos.html";
+        $projectName = "Proyectos";
+        include 'breadcrumbs.php';  
+        ?>
+
+        <?php
+        $galleryImages = [
+            '../images/Proyectos/Sub proyectos/PLASALUD/img1.png',
+            '../images/Proyectos/Sub proyectos/PLASALUD/img2.png',
+            '../images/Proyectos/Sub proyectos/PLASALUD/img3.png',
+            '../images/Proyectos/Sub proyectos/PLASALUD/img4.png'
+        ];
+        $projectTitle = "Plazas de Salud (PLASALUD) en Morelos, Guerrero y Ciudad de México";  
+        include 'content.php';  
+        ?>
+
+        <?php include 'footer.php'; ?>
+    </div>
+
+    <div class="snackbars" id="form-output-global"></div>
+    <script src="../js/core.min.js"></script>
+    <script src="../js/script.js"></script>
+</body>
+</html>
---
+++ Para la escuela/include/header.php
@@ -0,0 +1,40 @@
+<header class="section page-header rd-navbar-transparent-wrap">
+    <div class="rd-navbar-wrap">
+        <nav class="rd-navbar rd-navbar-transparent" data-layout="rd-navbar-fixed" data-sm-layout="rd-navbar-fixed" data-md-layout="rd-navbar-fixed" data-md-device-layout="rd-navbar-fixed" data-lg-layout="rd-navbar-static" data-lg-device-layout="rd-navbar-static" data-xl-layout="rd-navbar-static" data-xl-device-layout="rd-navbar-static" data-lg-stick-up-offset="44px" data-xl-stick-up-offset="44px" data-xxl-stick-up-offset="44px" data-lg-stick-up="true" data-xl-stick-up="true" data-xxl-stick-up="true"> 
+            <div class="rd-navbar-aside-outer rd-navbar-collapse">
+            </div>
+            <div class="rd-navbar-main-outer">
+                <div class="rd-navbar-main-inner">
+                    <div class="rd-navbar-main">
+                        <div class="rd-navbar-panel">
+                            <button class="rd-navbar-toggle" data-rd-navbar-toggle=".rd-navbar-nav-wrap"><span></span></button>
+                            <div class="rd-navbar-brand">
+                                <a class="brand" href="index.html">
+                                    <img class="brand-logo-dark" src="../images/logo-default-293x44.png" alt="" width="146" height="22"/>
+                                    <img class="brand-logo-light" src="../images/logo-inverse-310x44.png" alt="" width="155" height="22"/>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="rd-navbar-main-element">
+                            <div class="rd-navbar-nav-wrap">
+                                <ul class="rd-navbar-nav">
+                                    <li class="rd-nav-item"><a class="rd-nav-link" href="../index.html">Inicio</a></li>
+                                    <li class="rd-nav-item"><a class="rd-nav-link" href="../about.html">Acerca de</a></li>
+
+
+                                    <li class="rd-nav-item <?php echo $active_proyectos; ?>"><a class="rd-nav-link" href="../Proyectos.html">Proyectos</a></li>
+                                    <li class="rd-nav-item <?php echo $active_especialidades; ?>"><a class="rd-nav-link" href="../Especialidades.html">Especialidades</a></li>
+
+
+                                    <li class="rd-nav-item"><a class="rd-nav-link" href="../contacts.html">Contacto</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </div>
+    <div class="rd-navbar-bg novi-background bg-image"></div>
+</header>
+
