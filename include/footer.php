50f2cac7028ad46f1ae33e0d137104af8bbb5c53
Dopier8028 <diego.rafael.peralta@gmail.com>
2026-03-24 05:43:29 -0600

    first commit

---
+++ include/footer.php
@@ -0,0 +1,15 @@
+<footer class="section footer-classic">
+      <div class="footer-inner-1">
+        <div class="container">
+            <div class="col-md-5 col-lg-3">
+              <h5>Nuestro Contacto</h5>
+              <ul class="contact-list font-weight-bold">
+                <li>
+                  <div class="unit unit-spacing-xs">
+                    <div class="unit-left">
+                    </div>
+                    <div class="unit-body">diconova@gmail.com </div>
+                  </div>
+                </li>
+                <li>  
+    </footer>
